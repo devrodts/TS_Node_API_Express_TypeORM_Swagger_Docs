a5aa9ebaf92380fe2a7177692d7f1c727ceec505
rod <dev_rodts@outlook.com>
2025-02-07 08:15:12 -0300

    refactor: refactoring app.ts and add unknown errors handler

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -1,4 +1,4 @@
-import express, { Response, Request } from 'express'
+import express, { Response, Request, NextFunction } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import { AppError } from '@shared/errors/AppError'
@@ -8,20 +8,35 @@ import swaggerFile from '../../swagger.json'
 
 const app = express()
 
+
+// Middlewares
 app.use(express.json())
 app.use(cors())
+
+//API Routes 
 app.use(router)
+
+// Swagger Documentation
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
-app.use((error: Error, request: Request, response: Response) => {
+// Error Handling
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       body: {
         status: 'error',
         message: error.message,
+        statusCode: error.statusCode,
       },
     })
   }
+
+  //Unknown Error
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+    statusCode: 500,
+  })
 })
 
 export { app }
